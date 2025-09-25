8334db05f81ef905089b804046d282703f8abc2c
Brandi <brandi.davis2483@gmail.com>
2025-09-25 18:25:56 -0500

    [x] Finished the positioning of share modal on mobile breakpoint.
    [x] Updated image size/position for mobile breakpoint

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ console.log("Hello world");
 const btnShare = document.querySelector(".img-share");
 const imgWrapper = document.querySelector(".img-wrapper ");
 const modal = document.querySelector(".share-modal");
+const mobileQuery = window.matchMedia("(max-width: 43em)");
 
 btnShare.addEventListener("click", function () {
   modal.classList.toggle("hidden");
   imgWrapper.classList.toggle("svg-color");
+  if (mobileQuery.matches) {
+    imgWrapper.classList.toggle("active");
+  } else {
+    imgWrapper.classList.remove("active");
+  }
 });
+
+//on click take away the position:absolute for the img-wrapper
